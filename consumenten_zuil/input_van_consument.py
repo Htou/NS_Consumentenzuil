0521cf2bb89008808dd0e060d6445e7987a9b09d
mieke-lidia <FRANKA0V0@GMAIL.COM>
2019-10-29 13:56:57 +0100

    gefedge

--- consumenten_zuil/input_van_consument.py
+++ consumenten_zuil/input_van_consument.py
@@ -1,7 +1,7 @@
 while True:
-    invul = input("geef ons een berichtje :)")
+    invul = input("geef ons een berichtje: ")
     if len(invul) > 140:
-        print("ERROR give an imput of max 140 characters")
+        print("ERROR geef maximaal een input van 140 characters")
     else:
         break
 
