8f0f1eeac9c412c558b020046873924fad904641
hichem <h.touati@tutanita.com>
2019-10-29 15:56:29 +0100

    Refinements

--- GUI_consumentenzuil/GUI_consumentenzuil_twitterfeed.py
+++ GUI_consumentenzuil/GUI_consumentenzuil_twitterfeed.py
@@ -1,5 +1,6 @@
 import oauth2 as oauth
 
+
 def Request(url, key, secret, http_method='GET', post_body='', http_headers=''):
     consumer = oauth.Consumer(key='<your consumer key>', secret='<your consumer secret>')
     token = oauth.Token(key=key, secret=secret)
@@ -12,4 +13,3 @@ def Request(url, key, secret, http_method='GET', post_body='', http_headers=''):
         headers=http_headers)
 
     return request
-
--- Twitter API/Twitter_API.py
+++ Twitter API/Twitter_API.py
@@ -1,4 +1,3 @@
-
 import json
 
 credentials = {}
@@ -11,10 +10,11 @@ with open('twitter_credentials.json', 'w') as file:
     json.dump(credentials, file)
 
 from TwitterAPI import TwitterAPI
+
 api = TwitterAPI('IGS3kyp7lo1WoIaF9e1eUY8jK',
                  'AldbB9aspuKtKJghFJKHAlicth2s0dKABNqChFP9qmOHeiEyHg',
                  '1189156220092989443-BPUYdAsgmq1V08PWQtfZh3siFZ2Gwb',
                  'vggdKmRTfljwEms7virGlmg57PRE9hqBC5aZ4tFqfeHm0')
 
-r = api.request('statuses/update', {'status':'This is a tweet!'})
+r = api.request('statuses/update', {'status': 'Dit is een test tweet!'})
 print(r.status_code)
