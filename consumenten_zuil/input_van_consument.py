45282dbc9bd05d47313c2ff26295a2a05809df2e
hichem <h.touati@tutanita.com>
2019-10-29 14:11:13 +0100

    Docstrings toegevoegd

--- consumenten_zuil/input_van_consument.py
+++ consumenten_zuil/input_van_consument.py
@@ -1,4 +1,6 @@
 while True:
+    """"Input met maximaal 140 tekens, die naar berichten.txt schrijft"""
+
     invul = input("geef ons een berichtje: ")
     if len(invul) > 140:
         print("ERROR geef maximaal een input van 140 characters")
