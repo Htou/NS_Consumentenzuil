44b9f7f4ba319dc3671baf742e8335118ce34d8f
hichem <h.touati@tutanita.com>
2019-10-31 17:49:36 +0100

    Refinements

---
+++ GUI_consumentenzuil/GUI_stationshal.py
@@ -0,0 +1,20 @@
+from tkinter import *
+root = Tk()
+
+screenWidth = root.winfo_screenwidth()
+screenHeight = root.winfo_screenheight()
+
+root.overrideredirect(1)
+root.geometry('%dx%d+0+0' % (screenWidth, screenHeight))
+root.configure(background = '#000000') #black
+
+canvas = Canvas(
+    background='#000000',  # black
+    borderwidth=-5,
+    height=500,
+    relief='flat',
+    width=500)
+
+canvas.pack(expand=1, fill=BOTH)
+
+root.mainloop()
---
+++ consumenten_zuil/Request_response_tweets.py
@@ -0,0 +1,30 @@
+import oauth2 as oauth
+import json
+
+
+def Request(url, key, secret, http_method='GET', post_body=b'', http_headers=None):
+    consumer = oauth.Consumer(key="IGS3kyp7lo1WoIaF9e1eUY8jK",
+                              secret="AldbB9aspuKtKJghFJKHAlicth2s0dKABNqChFP9qmOHeiEyHg")
+    token = oauth.Token(key=key, secret=secret)
+    client = oauth.Client(consumer, token)
+    request = client.request(
+        url,
+        method=http_method,
+        body=post_body,
+        headers=http_headers)
+
+    return request
+
+
+request, response = Request(
+    'https://api.twitter.com/1.1/search/tweets.json?q=from%3AConsumentenzui1',
+    '1189156220092989443-BPUYdAsgmq1V08PWQtfZh3siFZ2Gwb',
+    'vggdKmRTfljwEms7virGlmg57PRE9hqBC5aZ4tFqfeHm0')
+
+data = json.loads(response)
+tweets = data['statuses']
+
+posts = list()
+for post in data['statuses']:
+    posts.append([post['text'], post['created_at'], post['user']['screen_name']])
+print(posts)
