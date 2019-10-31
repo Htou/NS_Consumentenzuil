608c0346039dd42130673812141852af80631fe7
hichem <h.touati@tutanita.com>
2019-11-01 00:01:54 +0100

    Refinements

--- GUI_consumentenzuil/GUI_stationshal.py
+++ GUI_consumentenzuil/GUI_stationshal.py
@@ -10,35 +10,34 @@ label = Label(master=root,
               font=('Helvetica', 16, 'bold italic'))
 
 label2 = Label(master=root,
-              text=fetch_tweets()[1],
-              background='blue',
-              foreground='yellow',
-              font=('Helvetica', 16, 'bold italic'))
+               text=fetch_tweets()[1],
+               background='blue',
+               foreground='yellow',
+               font=('Helvetica', 16, 'bold italic'))
 
 label3 = Label(master=root,
-              text=fetch_tweets()[2],
-              background='yellow',
-              foreground='blue',
-              font=('Helvetica', 16, 'bold italic'))
+               text=fetch_tweets()[2],
+               background='yellow',
+               foreground='blue',
+               font=('Helvetica', 16, 'bold italic'))
 
 label4 = Label(master=root,
-              text=fetch_tweets()[3],
-              background='blue',
-              foreground='yellow',
-              font=('Helvetica', 16, 'bold italic'))
+               text=fetch_tweets()[3],
+               background='blue',
+               foreground='yellow',
+               font=('Helvetica', 16, 'bold italic'))
 
 label5 = Label(master=root,
-              text=fetch_tweets()[4],
-              background='yellow',
-              foreground='blue',
-              font=('Helvetica', 16, 'bold italic'))
+               text=fetch_tweets()[4],
+               background='yellow',
+               foreground='blue',
+               font=('Helvetica', 16, 'bold italic'))
 
 label6 = Label(master=root,
-              text=fetch_tweets()[5],
-              background='blue',
-              foreground='yellow',
-              font=('Helvetica', 16, 'bold italic'))
-
+               text=fetch_tweets()[5],
+               background='blue',
+               foreground='yellow',
+               font=('Helvetica', 16, 'bold italic'))
 
 label.pack()
 label2.pack()
@@ -46,6 +45,5 @@ label3.pack()
 label4.pack()
 label5.pack()
 label6.pack()
-
 
 root.mainloop()
