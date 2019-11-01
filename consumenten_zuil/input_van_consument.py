47b84733355cc9a6dcf8cd8a8fc9291bef95d6d3
mieke-lidia <FRANKA0V0@GMAIL.COM>
2019-11-01 13:30:45 +0100

    1. Op een computer/zuil in het station kunnen mensen hun berichtje van maximaal 140 karakters invoeren. Het bericht komt vervolgens in een (tekst)bestand.

--- consumenten_zuil/input_van_consument.py
+++ consumenten_zuil/input_van_consument.py
@@ -1,12 +1,12 @@
-while True:
+while True: #de volgende loop word steeds herhaald tot er uit de loop word gebroken:
     """"Input met maximaal 140 tekens, die naar berichten.txt schrijft"""
 
-    invul = input("geef ons een berichtje: ")
-    if len(invul) > 140:
+    invul = input("geef ons een berichtje: ")   #de gebuiker kan een berichtje achterlaten
+    if len(invul) > 140:        #als het bericht meer dan 140 tekens bevat gebeurt het volgende:
         print("ERROR geef maximaal een input van 140 characters")
-    else:
-        break
+    else:   #als het minder of gelijk aan 140 tekens bevat gebeurt het volgende:
+        break   #break uit de loop
 
-outfile = open('berichten.txt', 'a')
-outfile.write(invul + '\n')
-outfile.close()
+outfile = open('berichten.txt', 'a')    #open het bestand berichten.txt met de functie append
+outfile.write(invul + '\n')         #schrijf in het bestand de waarde van invul en een enter
+outfile.close()     #sluit het bestand
