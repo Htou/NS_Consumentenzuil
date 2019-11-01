a7fa58c0b8e2c0791d8d64e34a17317d0719a7f7
mieke-lidia <FRANKA0V0@GMAIL.COM>
2019-11-01 13:50:27 +0100

    1. Op een computer/zuil in het station kunnen mensen hun berichtje van maximaal 140 karakters invoeren. Het bericht komt vervolgens in een (tekst)bestand.

--- consumenten_zuil/accept_reject3.py
+++ consumenten_zuil/accept_reject3.py
@@ -30,9 +30,9 @@ while True:
                 api = TwitterAPI('IGS3kyp7lo1WoIaF9e1eUY8jK',
                                  'AldbB9aspuKtKJghFJKHAlicth2s0dKABNqChFP9qmOHeiEyHg',
                                  '1189156220092989443-BPUYdAsgmq1V08PWQtfZh3siFZ2Gwb',
-                                 'vggdKmRTfljwEms7virGlmg57PRE9hqBC5aZ4tFqfeHm0')   #
+                                 'vggdKmRTfljwEms7virGlmg57PRE9hqBC5aZ4tFqfeHm0')   #twitterkeys, maakt het mogelijk om te plaatsen op switter
 
-                r = api.request('statuses/update', {'status': content})
+                r = api.request('statuses/update', {'status': content}) #plaats de inhoud van content (dus het bericht) op twitter
                 print(r.status_code)
 
                 break
